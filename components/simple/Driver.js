28ca9d6d51abe6417c64763bb159b9fc42e044b2
Jan Justi <9977169+janjusti@users.noreply.github.com>
2024-06-29 10:51:55 -0300

    added "catching" border to driver

--- components/simple/Driver.js
+++ components/simple/Driver.js
@@ -145,6 +145,7 @@ const Driver = ({
       <div
         style={{
           opacity: line.KnockedOut || line.Retired || line.Stopped ? 0.5 : 1,
+          border: line.IntervalToPositionAhead?.Catching ? "2px dashed green" : "none",
           display: "flex",
           height: "100%",
           alignItems: "center"
